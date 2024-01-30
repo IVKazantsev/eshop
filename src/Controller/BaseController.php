a12af56f59c2001478fea198a9eac46031f67791
Ilya Kazancev <ilyakazancev24112003@gmail.com>
2024-01-30 12:30:07 +0200

    refactor

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -7,15 +7,6 @@ abstract class BaseController
 {
 	public function render(string $templateName, array $params)
 	{
-		$template = __DIR__ . '/../View/' . $templateName;
-		$templateEngine = new TemplateEngine(ROOT . '/src/View/');
-
-		if (!file_exists($template))
-		{
-			http_response_code(404);
-			return $templateEngine->render('errorPage');
-		}
-
-		return $templateEngine->render($templateName, $params);
+		return (new TemplateEngine(__DIR__ . '../../src/View/'))->render($templateName, $params);
 	}
 }
