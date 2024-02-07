26f15ddab49800ea2d912b3def1f3f36eff723c3
Ilya Kazancev <ilyakazancev24112003@gmail.com>
2024-02-07 12:51:56 +0200

    debug phone validation

--- src/Service/ValidationService.php
+++ src/Service/ValidationService.php
@@ -7,12 +7,12 @@ class ValidationService
 	public static function validatePhoneNumber(string $phone): ?string
 	{
 		$phone = preg_replace('/\D/', '', $phone);
-		if (count($phone) === 10)
+		if (strlen($phone) === 10)
 		{
 			return '8' . $phone;
 		}
 
-		if (count($phone) !== 11)
+		if (strlen($phone) !== 11)
 		{
 			return null;
 		}
