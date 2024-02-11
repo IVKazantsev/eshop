22116754f3b8897b3789dfc5ead8bb65ca1fb9d6
Alexey Launert <alexmansup@gmail.com>
2024-02-11 18:06:41 +0200

    small changes to edit

--- src/View/components/table.php
+++ src/View/components/table.php
@@ -11,19 +11,20 @@ $fieldNames = $items[0]->getFieldNames();
 $iconsPath = Configurator::option('ICONS_PATH');
 $classname = $items[0]->getClassname();
 ?>
+<a href="<?= "/admin/{$classname}/add" ?>" class="add-entity-button">Добавить</a>
 <table class="admin-table">
 
 	<tr class="admin-table-header-row">
 		<?php foreach ($fieldNames as $fieldName): ?>
 			<th><?= $fieldName ?></th>
 		<?php endforeach; ?>
-		<th>Действия</th>
+		<th id="actions-column">Действия</th>
 	</tr>
 	<?php foreach ($items as $item): ?>
 		<tr class="admin-table-content-row">
 
 			<?php foreach ($fieldNames as $fieldName): ?>
-				<td class="field"><?= $item->getField($fieldName) ?></td>
+				<td class="<?= $fieldName ?>-field"><?= $item->getField($fieldName) ?></td>
 			<?php endforeach; ?>
 
 			<td class="actions-field">
--- src/View/pages/adminEditPage.php
+++ src/View/pages/adminEditPage.php
@@ -1,82 +1,83 @@
 <?php
 
+use N_ONE\App\Model\Entity;
 use N_ONE\App\Model\Item;
 
 /**
- * @var Item $item
+ * @var Entity $item
  */
-// var_dump($item);
+$fields = array_flip($item->getFieldNames(true));
+// var_dump($item->getField('tags')[0]->getTitle());
 ?>
 <div class="edit-form-container">
 	<form action="" class="edit-form" method="post">
 		<div class="form-section">
-
-			<p>ID товара: <?= $item->getId() ?></p>
-			<label for="title">
-				Название товара:
-				<input type="text" name="title" value="<?= $item->getTitle() ?>">
-			</label>
-			<label for="price">
-				Стоимость:
-				<input type="text" name="price" value="<?= $item->getPrice() ?>">
-			</label>
-			<label for="description">
-				Описание:
-				<textarea class="description-textarea" name="description"><?= $item->getDescription() ?></textarea>
-			</label>
+			<p>ID сущности: <?= $item->getId() ?></p>
+			<?php foreach ($fields as $field => $value): ?>
+				<?php if ($field === 'tags' || $field === 'images' || $field === 'id' || $field === 'dateCreate'): {
+					continue;
+				} endif ?>
+				<label for="<?= $field ?>">
+					<?= $field ?>:
+					<input type="text" name="<?= $field ?>" value="<?= $item->getField($field) ?>">
+				</label>
+			<?php endforeach; ?>
 		</div>
 		<div class="form-section">
-			<p>Теги:</p>
-			<div class="tag-group">
-				<p>Тип привода:</p>
-				<label for="drive-type">
-					<input type="radio" name="drive-type" value="Передний">
-					Передний
-				</label>
-				<label for="drive-type">
-					<input type="radio" name="drive-type" value="Задний">
-					Задний
-				</label>
-				<label for="drive-type">
-					<input type="radio" name="drive-type" value="Полный">
-					Полный
-				</label>
-			</div>
-			<div class="tag-group">
-				<p>Коробка передач:</p>
-				<label for="transmission-type">
-					<input type="radio" name="transmission-type" value="МКПП">
-					МКПП
-				</label>
-				<label for="transmission-type">
-					<input type="radio" name="transmission-type" value="АКПП">
-					АКПП
-				</label>
-			</div>
-			<div class="tag-group">
-				<p>Тип топлива:</p>
-				<label for="fuel-type">
-					<input type="radio" name="fuel-type" value="Бензин">
-					Бензин
-				</label>
-				<label for="fuel-type">
-					<input type="radio" name="fuel-type" value="Дизель">
-					Дизель
-				</label>
-			</div>
+			<?php if ($item instanceof Item): ?>
+				<p>Теги:</p>
+				<div class="tag-group">
+					<p>Тип привода:</p>
+					<label for="drive-type">
+						<input type="radio" name="drive-type" value="Передний">
+						Передний
+					</label>
+					<label for="drive-type">
+						<input type="radio" name="drive-type" value="Задний">
+						Задний
+					</label>
+					<label for="drive-type">
+						<input type="radio" name="drive-type" value="Полный">
+						Полный
+					</label>
+				</div>
+				<div class="tag-group">
+					<p>Коробка передач:</p>
+					<label for="transmission-type">
+						<input type="radio" name="transmission-type" value="МКПП">
+						МКПП
+					</label>
+					<label for="transmission-type">
+						<input type="radio" name="transmission-type" value="АКПП">
+						АКПП
+					</label>
+				</div>
+				<div class="tag-group">
+					<p>Тип топлива:</p>
+					<label for="fuel-type">
+						<input type="radio" name="fuel-type" value="Бензин">
+						Бензин
+					</label>
+					<label for="fuel-type">
+						<input type="radio" name="fuel-type" value="Дизель">
+						Дизель
+					</label>
+				</div>
 
-			<div class="tag-group">
-				<p>Тип двигателя:</p>
-				<label for="engine-type">
-					<input type="radio" name="engine-type" value="Турбированный">
-					Турбированный
-				</label>
-				<label for="engine-type">
-					<input type="radio" name="engine-type" value="Атмосферный">
-					Атмосферный
-				</label>
-			</div>
-			<button type="submit">Сохранить</button>
+				<div class="tag-group">
+					<p>Тип двигателя:</p>
+					<label for="engine-type">
+						<input type="radio" name="engine-type" value="Турбированный">
+						Турбированный
+					</label>
+					<label for="engine-type">
+						<input type="radio" name="engine-type" value="Атмосферный">
+						Атмосферный
+					</label>
+				</div>
+			<?php endif; ?>
+			<button class="submit-button" type="submit">Сохранить</button>
+
 		</div>
 	</form>
 </div>
