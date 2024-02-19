74bd37f77e5b75c20cd9a43785d7120c39374ac2
Ilya Kazancev <ilyakazancev24112003@gmail.com>
2024-02-19 17:11:55 +0200

    phone validity

--- public/js/checkingEmptyEntry.js
+++ public/js/checkingEmptyEntry.js
@@ -1,27 +1,29 @@
-// const form = document.getElementsByTagName("form")[0];
-// form.addEventListener("submit", function(event) {
-// 	const inputs = document.getElementsByTagName('input');
-// 	for (let i = 0; i < inputs.length; i++) {
-// 		let value = inputs[i].value.trim();
-// 		if (value === '') {
-// 			alert('Пожалуйста, заполните все поля ввода');
-// 			event.preventDefault();
-// 		}
-// 	}
-// });
-
 const inputs = document.getElementsByTagName('input');
 for (let i = 0; i < inputs.length; i++)
 {
 	inputs[i].addEventListener('input', function(event) {
 		let value = inputs[i].value.trim();
-		if(value !== '')
+		if (value !== '')
 		{
-			inputs[i].setCustomValidity("");
+			inputs[i].setCustomValidity('');
 		}
 		else
 		{
-			inputs[i].setCustomValidity("Поле не может быть пустым");
+			inputs[i].setCustomValidity('Поле не может быть пустым');
 		}
 	});
 }
+
+const phone = document.querySelector('input[type="tel"]');
+
+phone.addEventListener('input', (event) => {
+	const onlyNumbers = event.target.value.replace(/\D/g, '');
+	console.log(onlyNumbers);
+	if (onlyNumbers.length !== 11) {
+		// Показать ошибку с помощью Custom Validity
+		phone.setCustomValidity('Неверно введен номер телефона');
+	} else {
+		// Сбросить ошибку
+		phone.setCustomValidity('');
+	}
+});
